c0778ffec034f3f6c32269a3eebc209c46f5c0cd
AmelieMuller <amelie17.muller@gmail.com>
2023-04-27 16:38:47 +0200

    shop

---
+++ src/TsFiles/FirstTry.ts
@@ -0,0 +1,492 @@
+import { Scene, Engine, SceneLoader, FreeCamera, Vector3, HemisphericLight, SceneInstrumentation, MeshBuilder, AbstractMesh, Constants, Mesh, ActionManager, ExecuteCodeAction, PhysicsImpostor, int, AdvancedTimer, StandardMaterial, Texture, Vector4, Color3, Color4, Animation } from "@babylonjs/core";
+import "@babylonjs/loaders";
+import "@babylonjs/gui";
+import { AdvancedDynamicTexture, Button, Control, GUI3DManager, MeshButton3D, SelectionPanel, TextBlock } from "@babylonjs/gui";
+//import setAndStartTimer from "@babylonjs/Misc/timer";
+
+export class FirstTry {
+  scene: Scene;
+  engine: Engine;
+  cptLaine : int;
+  textBox : SelectionPanel;
+  text : TextBlock ;
+  mouton1 : boolean ;
+  available1 : string ;
+  timing : int ;
+  timersec : int ;
+  plane : Mesh;
+  matcollect : StandardMaterial;
+  avancement : int ;
+  camera : FreeCamera ;
+  cloth1: Cloth;
+  cloth2: Cloth;
+  cloth3: Cloth; 
+  cloth4: Cloth;
+  cloth5: Cloth; 
+  cloth6: Cloth;
+
+  //attribut pour les vetements:
+  wardrobe: Cloth[];
+  outfit: Cloth[];
+
+  constructor(private canvas: HTMLCanvasElement) {
+    this.engine = new Engine(this.canvas, true);
+
+    this.scene = this.CreateScene();
+
+    this.CreateEnvironment();
+
+    this.camera=new FreeCamera("camera",new Vector3(0,10,0),this.scene);
+
+    this.CreateController();
+
+    this.CreateChooseYourOutfit();
+
+    
+    //Setup pour le cpt de Laine
+    this.textBox = new SelectionPanel("textBox");
+    this.text = new TextBlock();
+    this.cptLaine=0;
+    this.mouton1 = true;
+    this.available1 = "Collect your yarn !";
+    this.timing = 0 ;
+    this.avancement = 1;
+    this.timersec = 5 ;
+    const f = new Vector4(0,0, 1 , 1);
+    this.plane = MeshBuilder.CreatePlane("plane", {frontUVs: f, backUVs: f, sideOrientation: Mesh.DOUBLESIDE});
+    this.matcollect = new StandardMaterial("",this.scene);
+    this.matcollect.diffuseTexture = new Texture("./textures/timer/collect.png");
+    
+
+    this.CreateCptLaine();
+    this.CreateMouton();
+    
+    this.CreatePersonnage();
+
+    //pour les vetements:
+    this.wardrobe = [];
+    this.outfit = [];
+    this.cloth1 = new Cloth("manche", 7);
+    this.cloth2 = new Cloth("bob", 3);
+    this.cloth3 = new Cloth("fleur_bleue", 6);
+    this.cloth4 = new Cloth("fleur_blanc", 6);
+    this.cloth5 = new Cloth("long_blanc", 6);
+    this.cloth6 = new Cloth("long_bleu", 6);
+
+
+    //this.CreateCutScene();
+    this.CreateStartRunway();
+
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+  }
+  
+
+  CreateScene(): Scene {
+    const scene = new Scene(this.engine);
+    const light= new HemisphericLight("light",new Vector3(0,1,0),this.scene);
+
+    scene.onPointerDown=(evt)=>{
+        //for the mouse settings, 0:left click,1:middle mouse button,2:right click
+        if (evt.button===0) this.engine.enterPointerlock();
+        if (evt.button===1) this.engine.exitPointerlock();
+    }
+    const framesPerSecond=60;
+    const gravity=-9.81;
+    //gravity on the y axis
+    scene.gravity=new Vector3(0,gravity/framesPerSecond,0);
+    //enable collisions
+    scene.collisionsEnabled=true;
+
+
+    this.CreateActions(this);
+
+    return scene;
+  }
+
+  async CreateMouton(): Promise<void> {
+    
+    const { meshes } = await SceneLoader.ImportMeshAsync(
+      "",
+      "./models/",
+      "moutonGwen.glb",
+      this.scene
+    );
+    //apply collisions to every mesh in the model
+    //.map goes through every mesh
+    let i =0;
+    meshes.map(mesh=>{ //for each mesh apply collisions donc c'est comme un for i in list par exemple
+       mesh.checkCollisions=true;
+       //console.log("meshes : "+i+" "+mesh);
+       i++;
+      //Création de l'intéraction avec mouton
+      mesh.actionManager = new ActionManager(this.scene);
+      mesh.actionManager.registerAction(
+        new ExecuteCodeAction({trigger: ActionManager.OnPickTrigger},(evt) => this.Mouton1OnClick(this)));  //Quand on click sur la boule ca lance Mouton1OnClick
+
+    })
+
+    //Plan 2D qui donne des infos au dessus
+    //const plane = Mesh.CreatePlane("plane",2,this.scene); //plane, le plan 2D sur lequel on va cliquer, 2=size
+    //plane.parent = meshes[0];
+    //plane.position.y = 2;
+
+    
+    //const matcollect = new StandardMaterial("",this.scene);
+    //matcollect.diffuseTexture = new Texture("./textures/timer/collect.png");   ////./textures/timer/collect.png
+    //textures pour la barres qui augmente
+    const matbarre = new StandardMaterial("",this.scene);
+    matbarre.diffuseTexture = new Texture("./textures/timer/barre1.png");
+    
+    
+    //const f = new Vector4(0,0, 1 , 1); // front image = half the whole image along the width 
+    //const b = new Vector4(1,0, 1, 1); // back image = second half along the width
+    
+    //const plane = MeshBuilder.CreatePlane("plane", {frontUVs: f, backUVs: f, sideOrientation: Mesh.DOUBLESIDE});
+    this.plane.parent = meshes[1];
+    this.plane.position.y = 2;
+    this.plane.scaling.x=4;
+    this.plane.scaling.y=1;
+    this.plane.rotate(new Vector3(0,1,0),-1.5708);
+    this.plane.material = this.matcollect;
+  }
+
+  Mouton1OnClick(self : FirstTry):void{
+    if (self.mouton1){
+      self.cptLaine+=1;
+      self.text.text = "laine : "+self.cptLaine;
+      self.mouton1=false;
+      console.log("Juste avant timer");
+      //button.textBlock!.text = "Please wait to collect your yarn";
+      const timer = new AdvancedTimer({timeout:1000,contextObservable: self.scene.onBeforeRenderObservable});  //Timer à 0 jsp pk mais j'ai pas vu de changements en fonctions des valeurs
+      timer.onTimerEndedObservable.add((evt) => self.Timer(self));
+      timer.onEachCountObservable.add((evt) => self.Waiting(self));
+      timer.start(this.timersec*1000); //La durée du timer
+      
+    }
+    self.textBox.addControl(self.text);
+
+  }
+
+  CreateCptLaine():void{
+    const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+    this.textBox.width = 0.25;
+    this.textBox.height = 0.10;
+    this.textBox.background = "#1388AF";
+    this.textBox.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
+    this.textBox.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+        
+    this.text.text = "laine : "+ this.cptLaine;
+    this.text.color = "white";
+    this.text.fontSize = 30;
+    this.textBox.addControl(this.text);  
+    advancedTexture.addControl(this.textBox);
+  }
+
+  Timer(self : FirstTry) : void{
+    self.mouton1=true;
+    //button.textBlock!.text = this.available1 ;
+    self.plane.material = self.matcollect ;
+    self.timing=0;
+    self.avancement = 1 ;
+  }
+
+  Waiting(self : FirstTry) : void{
+    self.mouton1=false;
+    if (self.timing%(Math.trunc(self.timersec*1000/340))==0&&self.avancement<=17){
+      const matbarre = new StandardMaterial("",this.scene);
+      matbarre.diffuseTexture = new Texture("./textures/timer/barre"+self.avancement+".png");
+      // mettre self.timersec/340 en entier 
+      self.plane.material = matbarre ;
+      //console.log("la valeur : "+Math.trunc(self.timersec*1000/340));
+      self.avancement+=1;
+      //matbarre.diffuseTexture = new Texture("./textures/timer/barre1.png");
+      //button.textBlock!.text = `Wait ${self.timersec - (self.timing/(self.timersec*10))} seconds to collect your next yarn` ;
+    }
+    self.timing+=1;
+     
+  }
+
+  async CreateEnvironment(): Promise<void> {
+    
+    const { meshes } = await SceneLoader.ImportMeshAsync(
+      "",
+      "./models/",
+      "withRunway.glb",
+      this.scene
+    );
+
+    //apply collisions to every mesh in the model
+    //.map goes through every mesh
+    meshes.map(mesh=>{ //for each mesh apply collisions donc c'est comme un for i in list par exemple
+       mesh.checkCollisions=true;
+    })
+
+  }
+
+  CreateController():void{
+    //const camera=new FreeCamera("camera",new Vector3(0,10,0),this.scene); //without a camera we can't see anything
+    //static camera unless we attach control
+    this.camera.attachControl();
+    
+
+    this.camera.applyGravity=true; //applies gravity to the camera which is our controller
+    this.camera.checkCollisions=true;
+    //but this will not work unless we can "detect" the camera, we need to create a 'body' for the camera => elipsoid 
+
+    this.camera.ellipsoid=new Vector3(1,1,1);
+
+    this.camera.minZ=0.45; //this allows us to not get very close to the objects and see through them
+    this.camera.speed=0.5;
+    this.camera.angularSensibility=4000; //rotate more slowly
+
+    //personalize the keys for the controller movement
+    //we can still use arrows keys for movement donc both options are available
+    this.camera.keysUp.push(87); //keycode for W is 87 check https://www.toptal.com/developers/keycode
+    this.camera.keysUp.push(90); // 90 : z pour les azerty : dsl hajar
+    this.camera.keysRight.push(68); //65: d
+    this.camera.keysDown.push(83);//83:s
+    this.camera.keysLeft.push(81);//81:q
+
+    
+
+    }
+    /*
+    CreateObjects():void {
+      const ball = MeshBuilder.CreateSphere("ball",{diameter : 1} , this.scene);
+      /*ball.physicsImpostor = new PhysicsImpostor(
+          ball,
+          PhysicsImpostor.SphereImpostor,
+          { mass: 1, restitution: 0.8 }
+        );
+        
+      ball.position=new Vector3(0,1,1);
+      this.CreateActions(ball);
+
+  }
+  */
+
+  CreateActions(self : FirstTry): void {
+    const ball = MeshBuilder.CreateSphere("ball",{diameter : 1} , this.scene);
+    ball.position=new Vector3(0,1,1);
+      ball.actionManager = new ActionManager(this.scene);
+      ball.actionManager.registerAction(
+          new ExecuteCodeAction({trigger: ActionManager.OnPickTrigger}, shop)
+          );
+      function shop() {
+        (document.querySelector(".modal-wrapper") as HTMLDivElement).style.display = "block";  //AFFICHE LA PAGE SHOP
+        (document.querySelector(".modal-close") as HTMLDivElement).addEventListener("click", hide);  //Clique de la croix ?
+        (document.getElementById("manche") as HTMLButtonElement)!.addEventListener("click",() => buy("manche",self));  //buy cloth1
+
+        (document.querySelector("#fleur_bleu") as HTMLButtonElement)!.addEventListener("click",() => buy("fleur_bleu",self));
+        (document.querySelector("#fleur_blanc") as HTMLButtonElement)!.addEventListener("click",() => buy("fleur_blanc",self));
+        (document.querySelector("#long_blanc") as HTMLButtonElement)!.addEventListener("click",() => buy("long_blanc",self));
+        (document.querySelector("#long_marron") as HTMLButtonElement)!.addEventListener("click",() => buy("long_marron",self));
+        (document.querySelector("#bob") as HTMLButtonElement)!.addEventListener("click",() => buy("bob", self));
+      
+      function hide() {
+          (document.querySelector(".modal-wrapper") as HTMLDivElement).style.display = "none";  //Enlève la page shop
+          }
+      function buy(name: string, self: FirstTry){
+        if(isOwned(name)==true){
+          alert("You already own "+name);
+        }
+        else{
+          console.log(name);
+          let price = 6;
+          if(name=="bob"){
+            price = 3;
+          }
+          const cloth = new Cloth(name, price);
+          if((self.cptLaine >= cloth.price)){
+            self.cptLaine = self.cptLaine-cloth.price;
+            self.text.text = "laine : "+self.cptLaine;
+            cloth.owned = true;
+            self.wardrobe.push(cloth);
+            alert("You just bought "+cloth.name);
+            }
+           
+          else{
+             alert("You dont have enought wool, soory :(");
+          }
+        }
+      
+      }   
+    
+
+      function isOwned(name: string){
+        for(const c of self.wardrobe){
+          if(c.name==name){
+            return true;
+          }
+        }
+      }
+    }
+    }
+    
+    CreateChooseYourOutfit():void {
+      const plane = Mesh.CreatePlane("plane",3,this.scene); //plane, le plan 2D sur lequel on va cliquer, 2=size
+      plane.position.y = 2;
+      plane.position.x = -45;
+      plane.position.z = 28;
+      plane.rotate(new Vector3(0,1,0),-1.5708);
+  
+      const advancedTexture2 = AdvancedDynamicTexture.CreateForMesh(plane);
+  
+      const button1 = Button.CreateSimpleButton("but1", "Choose your outfit");
+      button1.width = 1;
+      button1.height = 0.4;
+      button1.color = "black";
+      button1.fontSize = 50;
+      button1.background = "pink";
+      button1.onPointerUpObservable.add(()=>this.ClickOutfit(this));
+      advancedTexture2.addControl(button1);
+      
+    }
+    ClickOutfit(self : FirstTry):void{
+      (document.querySelector(".modal-wrapper-outfit") as HTMLDivElement).style.display = "block";  //AFFICHE LA PAGE SHOP
+      (document.querySelector(".modal-close-outfit") as HTMLDivElement).addEventListener("click", hide);  //Clique de la croix ?
+      /*
+      (document.querySelector(".modal-close-outfit") as HTMLDivElement).addEventListener("click", hide); 
+      (document.querySelector(".modal-close-outfit") as HTMLDivElement).addEventListener("click", hide);
+      (document.querySelector(".modal-close-outfit") as HTMLDivElement).addEventListener("click", hide);
+      */
+      function hide() {
+          (document.querySelector(".modal-wrapper-outfit") as HTMLDivElement).style.display = "none";  //Enlève la page shop
+          }
+      /*
+      function wear(){
+
+      }
+      */
+    }
+    CreateCutScene(self : FirstTry):void{
+      const camKeys = [];
+      console.log("Dans la methode",self);
+      const fps = 60;
+      const camAnim = new Animation(
+        "camAnim",
+        "position",
+        fps,Animation.ANIMATIONTYPE_VECTOR3,
+        Animation.ANIMATIONLOOPMODE_CONSTANT,
+        true);
+
+      camKeys.push({frame:0 , value: new Vector3(-15,3,-20)});
+      //camKeys.push({frame:5* fps , value: new Vector3(-35,2,-20)});
+      camKeys.push({frame:8* fps , value: new Vector3(-28,3,-20)});
+      //camKeys.push({frame:8* fps +1 , value: new Vector3(-28,4,-23)});
+      //camKeys.push({frame:16* fps , value: new Vector3(-15,4,-23)});
+      camAnim.setKeys(camKeys);
+      self.camera.detachControl();
+      self.camera.position = new Vector3(-15,3,-20);
+      self.camera.rotation = new Vector3(0,Math.PI,0);
+      self.camera.minZ=0.45; //this allows us to not get very close to the objects and see through them
+      self.camera.speed=0.5;
+      self.camera.animations.push(camAnim);
+      self.scene.beginAnimation(self.camera, 0,8* fps);
+      const timer = new AdvancedTimer({timeout:8* fps,contextObservable: self.scene.onBeforeRenderObservable});  //Timer à 0 jsp pk mais j'ai pas vu de changements en fonctions des valeurs
+      timer.onTimerEndedObservable.add(() => self.SecondAnimation(self));
+      timer.start(8* fps*18);
+    }
+    SecondAnimation(self : FirstTry){
+      const fps = 60;
+      const camKeys = [];
+      const camAnim = new Animation(
+        "camAnim",
+        "position",
+        fps,Animation.ANIMATIONTYPE_VECTOR3,
+        Animation.ANIMATIONLOOPMODE_CONSTANT,
+        true);
+
+      camKeys.push({frame:0, value: new Vector3(-40,5.5,-26)});
+      camKeys.push({frame:2* fps, value: new Vector3(-40,5.5,-26)});
+      camKeys.push({frame:10* fps , value: new Vector3(-20,5.5,-26)});
+      camAnim.setKeys(camKeys);
+      self.camera.rotation = new Vector3(Math.PI/8,Math.PI/2,0);
+      self.camera.position = new Vector3(-40,5.5,-26);
+      self.camera.animations.slice(0,self.camera.animations.length);
+      self.camera.animations.push(camAnim);
+      self.scene.beginAnimation(self.camera, 0 ,10* fps);
+      const timer = new AdvancedTimer({timeout:8* fps,contextObservable: self.scene.onBeforeRenderObservable});  //Timer à 0 jsp pk mais j'ai pas vu de changements en fonctions des valeurs
+      timer.onTimerEndedObservable.add(() => self.AfterCutScene(self));
+      timer.start(10* fps*18);
+    }
+
+    AfterCutScene(self : FirstTry){
+      self.camera.position = new Vector3(-10,2,-29);
+      self.camera.attachControl();
+      self.camera.applyGravity=true; //applies gravity to the camera which is our controller
+      self.camera.checkCollisions=true;
+      //but this will not work unless we can "detect" the camera, we need to create a 'body' for the camera => elipsoid 
+
+      self.camera.ellipsoid=new Vector3(1,1,1);
+
+      self.camera.minZ=0.45; //this allows us to not get very close to the objects and see through them
+      self.camera.speed=0.5;
+      self.camera.angularSensibility=4000; //rotate more slowly
+      self.camera.keysUp.push(87); //keycode for W is 87 check https://www.toptal.com/developers/keycode
+      self.camera.keysUp.push(90); // 90 : z pour les azerty : dsl hajar
+      self.camera.keysRight.push(68); //65: d
+      self.camera.keysDown.push(83);//83:s
+      self.camera.keysLeft.push(81);//81:q
+
+    }
+
+    async CreatePersonnage(): Promise<void> {
+      const { meshes , animationGroups } = await SceneLoader.ImportMeshAsync(
+        "",
+        "./models/",
+        "animated.glb",
+        this.scene
+      );
+      meshes[0].rotate(Vector3.Up(),Math.PI/2);
+      meshes[0].position = new Vector3(-15,1.65,-26.5);
+      meshes[0].scaling = new Vector3(2,2,2);
+      
+      //console.log("Animation group : ", animationGroups);
+
+      animationGroups[0].stop();
+      animationGroups[2].play(true);
+    }
+
+    CreateStartRunway():void {
+      const plane = Mesh.CreatePlane("plane",3,this.scene); //plane, le plan 2D sur lequel on va cliquer, 2=size
+      plane.position.y = 2;
+      plane.position.x = -9;
+      plane.position.z = -26.5;
+      plane.rotate(new Vector3(0,1,0),-1.5708);
+  
+      const advancedTexture2 = AdvancedDynamicTexture.CreateForMesh(plane);
+  
+      const button1 = Button.CreateSimpleButton("but1", "Start your runway");
+      button1.width = 1;
+      button1.height = 0.4;
+      button1.color = "black";
+      button1.fontSize = 50;
+      button1.background = "pink";
+      advancedTexture2.addControl(button1);
+      console.log("Juste avant",this);
+      button1.onPointerUpObservable.add(() => this.CreateCutScene(this));
+      
+      
+    }
+    
+}
+
+class Cloth{
+  name: string;
+  price: int;
+  worn : boolean;
+  owned: boolean;
+
+  constructor(n:string, p: int ){
+    this.name = n;
+    this.price = p;
+    this.worn = false;
+    this.owned = false;
+  }
+
+}
+
---
+++ src/components/MyGame.vue
@@ -0,0 +1,306 @@
+<template>
+
+  <canvas></canvas>
+  <div class="modal-wrapper">
+     <div class="modal">
+         <div class="modal-close">X</div>
+         <div class="modal-content">
+             <h2 id="titre-shop">Let's shop</h2>
+             <div id="un">
+                <img class="img" src="./image/newpose_manche.png">
+                <button class="bog" id="manche">Buy on game</button>
+                <button><a href="https://www.instagram.com/">Buy irl !</a></button>
+            </div>
+
+            <div id="deux">
+                <img class="img" src="./image/newpose_top_fleur_blanc.png">
+                <button class="bog" id="fleur_blanc">Buy on game</button>
+                <button><a href="https://www.instagram.com/">Buy irl !</a></button>
+            </div>
+
+            <div id="trois">
+                <img class="img" src="./image/newpose_top_fleur_bleu.png">
+                <button class="bog" id="fleur_bleu">Buy on game</button>
+                <button><a href="https://www.instagram.com/">Buy irl !</a></button>
+            </div>
+
+            <div id="quatre">
+                <img class="img" src="./image/newpose_top_long_blanc.png">
+                <button class="bog" id="long_blanc">Buy on game</button>
+                <button><a href="https://www.instagram.com/">Buy irl !</a></button>
+            </div>
+
+            <div id="cinq">
+                <img class="img" src="./image/newpose_top_long_marron.png">
+                <button class="bog" id="long_marron">Buy on game</button>
+                <button><a href="https://www.instagram.com/">Buy irl !</a></button>
+            </div>
+
+            <div id="six">
+                <img class="img" src="./image/newbob.png">
+                <button class="bog" id="bob">Buy on game</button>
+                <button><a href="https://www.instagram.com/">Buy irl !</a></button>
+            </div>
+
+        
+         </div>
+     </div>
+ </div>
+ <div class="modal-wrapper-outfit">
+     <div class="modal-outfit">
+         <div class="modal-close-outfit">X</div>
+         <div class="modal-content-outfit">
+             <h2>Choose your outfit</h2>
+             <p>Welcome to your closet!</p>
+             <a href="https://www.instagram.com/">Buy in real life!</a>
+             <button class="AV1-outfit">Buy on game</button>
+         </div>
+     </div>
+ </div>
+
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FirstTry} from '@/TsFiles/FirstTry' ;
+export default defineComponent({
+  name: 'BabylonExamples',
+  mounted(){
+    const canvas=document.querySelector("canvas")!;
+    new FirstTry(canvas);
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only partie css-->
+<style scoped>
+canvas {
+  width:100% ;
+  height:100% ;
+}
+.modal-wrapper {
+  background: rgba(0, 0, 0, 0.508);
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: none;
+}
+
+
+.modal {
+  text-align: center;
+  max-width: 800px;
+  width: 100%;
+  background: rgb(183, 194, 207);
+  padding: 20px;
+  margin: auto;
+  margin-top: 20px;
+  height: 600px;
+  border-radius: 5px;
+  position: relative;
+}
+
+.modal a {
+  text-decoration: none;
+  background: rgb(197, 34, 66);
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin: 10px;
+
+}
+
+.modal-content {
+  display: grid;
+  grid-template-rows: 40px 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 5px;
+  height: 600px;
+}
+
+#titre-shop{
+    grid-column: 2;
+    grid-row: 1;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    margin: auto;
+    color: rgb(109, 12, 32);
+}
+
+.modal-close {
+  color: rgb(197, 34, 66);
+  border: 1px solid black;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  cursor: pointer;
+  width: 20px;
+  border-radius: 5px;
+}
+
+
+button{
+    outline: 0;
+    grid-gap: 8px;
+    align-items: center;
+    background-color: rgb(197, 34, 66);
+    color: rgb(250, 248, 248);
+    border: 1px solid #000;
+    border-radius: 4px;
+    cursor: pointer;
+    display: inline-flex;
+    flex-shrink: 0;
+    font-size: 10px;
+    gap: 8px;
+    justify-content: center;
+    line-height: 1.5;
+    overflow: hidden;
+    padding: 12px 16px;
+    text-decoration: none;
+    text-overflow: ellipsis;
+    transition: all .14s ease-out;
+    white-space: nowrap;
+    width: 70px;
+    height: 10px;
+}
+
+button:hover {
+    box-shadow: 4px 4px 0 #000;
+    transform: translate(-4px,-4px);
+}
+
+button:focus-visible{
+    outline-offset: 1px;
+}
+            
+.img{
+    grid-row: 1;
+    grid-column: 1/3;
+    width: 150px;
+    margin: auto;
+}
+
+.bog{
+    
+    margin-right: 0px;
+    margin-left: 55px;
+}
+
+#un{
+    grid-row: 2;
+    grid-column: 1;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 4fr 1fr;
+    gap: 10px;
+}
+
+#deux{
+    grid-row: 2;
+    grid-column: 2;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 4fr 1fr;
+    gap: 10px;
+}
+
+#trois{
+    grid-row: 2;
+    grid-column: 3;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 4fr 1fr;
+    gap: 10px;
+}
+
+#quatre{
+    grid-row: 3;
+    grid-column: 1;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 4fr 1fr;
+    gap: 10px;
+}
+
+#cinq{
+    grid-row: 3;
+    grid-column: 2;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 4fr 1fr;
+    gap: 10px;
+}
+
+#six{
+    grid-row: 3;
+    grid-column: 3;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 4fr 1fr;
+    gap: 10px;
+}
+
+
+
+/*Pour le choose your outfit */
+
+.modal-wrapper-outfit {
+  background: rgba(0, 0, 0, 0.508);
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  display: none;
+}
+.modal-outfit {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  max-width: 600px;
+  width: 100%;
+  background: wheat;
+  padding: 20px;
+  margin: 35vh auto;
+  border-radius: 5px;
+  position: relative;
+}
+.modal-outfit a {
+  text-decoration: none;
+  background: crimson;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin: 10px;
+}
+.modal-close-outfit {
+  color: red;
+  border: 1px solid black;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  cursor: pointer;
+  width: 20px;
+  border-radius: 5px;
+}
+/*
+button {
+  background: crimson;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin: 10px auto;
+  cursor: pointer;
+}
+
+.AV1-outfit {
+  background: crimson;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin: 10px auto;
+  cursor: pointer;
+  }
+  */
+</style>
